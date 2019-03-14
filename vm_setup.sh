cb0b76f5123c109d4925968c2655a9c581912f14
Tanner Prestegard <tprestegard@gmail.com>
2019-03-14 16:15:07 -0500

    Add kubectl installation

--- vm_setup.sh
+++ vm_setup.sh
@@ -192,6 +192,13 @@ cd $HOME/linux-setup
 git config --local user.email "tprestegard@gmail.com"
 echo "DONE"
 
+# Install kubectl
+sudo apt-get update && sudo apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
+
 # Things to do manually at the end
 echo ""
 read -d '' STUFF <<"EOF"

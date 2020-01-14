0209649db530f6d05920321c4088cc4b5cbd297e
Tanner Prestegard <tprestegard@gmail.com>
2020-01-14 10:03:41 -0600

    more flexible ssh keys for demo_ssh_or_scp func

--- files/functions/demo_ssh_or_scp.fish
+++ files/functions/demo_ssh_or_scp.fish
@@ -3,13 +3,12 @@ function demo_ssh_or_scp
     set cmd "$argv[1]"
     test -n "$argv[2]"; and set CLOUD_PROVIDER "$argv[2]"; or set CLOUD_PROVIDER "aws"
     if test "$CLOUD_PROVIDER" = "aws"
-        set KEY_NAME "navops-launch-demo-tanner.pem"
+        set SSH_KEY (ls $HOME/univa/navops-launch/demo/aws/.ssh/*.pem)
     else if test "$CLOUD_PROVIDER" = "azure"
-        set KEY_NAME "id_rsa"
+        set SSH_KEY (ls $HOME/univa/navops-launch/demo/azure/.ssh/id_rsa)
     else
         echo "error: cloud provider should be 'aws' or 'azure'"
         return 1
     end
-    set SSH_KEY "$HOME/univa/navops-launch/demo/$CLOUD_PROVIDER/.ssh/$KEY_NAME"
     $cmd -o IdentitiesOnly=yes -i $SSH_KEY $argv[3..-1]
 end

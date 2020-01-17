7cc062e9f1a4d478c9e4da4a858ef7580079612e
Tanner Prestegard <tprestegard@gmail.com>
2020-01-17 09:43:22 -0600

    one small update to demo_ssh_or_scp

--- files/functions/demo_ssh_or_scp.fish
+++ files/functions/demo_ssh_or_scp.fish
@@ -3,7 +3,7 @@ function demo_ssh_or_scp
     set cmd "$argv[1]"
     test -n "$argv[2]"; and set CLOUD_PROVIDER "$argv[2]"; or set CLOUD_PROVIDER "aws"
     if test "$CLOUD_PROVIDER" = "aws"
-        set SSH_KEY (ls $HOME/univa/navops-launch/demo/aws/.ssh/*.pem)
+        set SSH_KEY (ls $HOME/univa/navops-launch/demo/aws/.ssh/*.pem | head -1)
     else if test "$CLOUD_PROVIDER" = "azure"
         set SSH_KEY (ls $HOME/univa/navops-launch/demo/azure/.ssh/id_rsa)
     else

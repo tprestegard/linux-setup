52a37d12707504df709f0ec74176e62e09587298
Tanner Prestegard <tprestegard@gmail.com>
2020-03-11 20:01:28 -0500

    gcp_rsync fish function

---
+++ files/functions/gcp_rsync.fish
@@ -0,0 +1,10 @@
+function gcp_rsync
+
+    set IP "$argv[1]"
+    set SRC_DIR "$argv[2]"
+    set TARGET_DIR "$argv[3]"
+    test -n "$argv[4]"; and set USERNAME "$argv[4]"; or set USERNAME "root"
+    set SSH_KEY "$HOME/.ssh/id_rsa_univa"
+
+    rsync -av --delete -e "ssh -i $SSH_KEY" $DIR "$USERNAME@$IP:$TARGET_DIR"
+end

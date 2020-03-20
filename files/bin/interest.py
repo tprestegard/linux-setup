e400b1fa2e0c34940492c9c4481b3defc8a70d2d
Tanner Prestegard <tprestegard@gmail.com>
2020-03-20 15:44:15 -0500

    python3-ify interest functions

--- files/bin/interest.py
+++ files/bin/interest.py
@@ -33,44 +33,44 @@ N = args.loan_term*12
 M = P0*i*(1+i)**N / ((1+i)**N - 1)
 # Check computation, if desired.
 if (args.check):
-    print "Checking monthly payment computation...",
+    print("Checking monthly payment computation...")
     P_rem = P0
     for j in range(1,N+1):
         P_rem = P_rem*(1+i) - M
     if (abs(P_rem) < 1e-6):
-        print "OK."
+        print("OK.")
     else:
-        print "not OK!"
+        print("not OK!")
         raise ValueError("Monthly payment computation is wrong.")
 
 # Compute total paid.
 total = M*N + args.down_payment
 if (args.check):
-    print "Checking total paid...",
+    print("Checking total paid...")
     tot_check = P0*i*N * (1+i)**N / ((1+i)**N - 1) + args.down_payment
     if (abs(total-tot_check) < 1e-6):
-        print "OK."
+        print("OK.")
     else:
-        print "not OK!"
+        print("not OK!")
         raise ValueError("Total paid computation is wrong.")
 
 # Compute total interest paid.
 tot_int = total - args.price
 if (args.check):
-    print "Checking total interest...",
+    print("Checking total interest...")
     ti_check = P0*(i*N*(1+i)**N/((1+i)**N - 1) - 1)
     if (abs(tot_int - ti_check) < 1e-6):
-        print "OK."
+        print("OK.")
     else:
-        print "not OK!"
+        print("not OK!")
         raise ValueError("Total interest computation is wrong.")
 
 # Print results.
-print "House price:\t\t$%d" % args.price
-print "Down payment:\t\t$%d"% args.down_payment
-print "Interest rate:\t\t %.3f%%" % (args.interest_rate*100)
-print "Monthly payment:\t$%.2f" % M
-print "Total paid:\t\t$%.2f" % total
-print "Total interest paid:\t$%.2f" % tot_int
+print("House price:\t\t$%d" % args.price)
+print("Down payment:\t\t$%d"% args.down_payment)
+print("Interest rate:\t\t %.3f%%" % (args.interest_rate*100))
+print("Monthly payment:\t$%.2f" % M)
+print("Total paid:\t\t$%.2f" % total)
+print("Total interest paid:\t$%.2f" % tot_int)
 
 # End of file

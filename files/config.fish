098d5c5e25460ec9f7382259ce31ddadd7d937ca
Tanner Prestegard <tprestegard@gmail.com>
2020-04-10 07:56:59 -0500

    Add $HOME/.local/bin to $PATH

--- files/config.fish
+++ files/config.fish
@@ -1,6 +1,9 @@
 # Set up virtualfish
 eval (python3 -m virtualfish auto_activation)
 
+# Add $HOME/.local/bin
+set PATH $HOME/.local/bin $PATH
+
 # Set up arrow keys for history completion
 bind -k up history-token-search-backward
 bind -k down history-token-search-forward

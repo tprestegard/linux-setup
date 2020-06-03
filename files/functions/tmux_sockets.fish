8f06029f07d0cc48bb50b01b2218c800fe63c94f
Tanner Prestegard <tprestegard@gmail.com>
2020-06-02 20:55:13 -0500

    Fix to tmux_sockets function

--- files/functions/tmux_sockets.fish
+++ files/functions/tmux_sockets.fish
@@ -1,5 +1,5 @@
 function tmux_sockets
-    for socket in (lsof -U | grep tmux-1000/ | sed 's/.*tmux-1000\/\([0-9]*\) .*/\1/' | uniq)
+    for socket in (lsof -U 2> /dev/null | grep tmux-1000/ | sed 's/.*tmux-1000\/\([0-9]*\) .*/\1/' | uniq)
         echo $socket
     end
 end

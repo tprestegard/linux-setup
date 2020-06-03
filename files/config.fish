1f3a1be484c166d3320fcdc61c5cc74715103d16
Tanner Prestegard <tprestegard@gmail.com>
2020-06-02 20:55:04 -0500

    Add EDITOR config to config.fish

--- files/config.fish
+++ files/config.fish
@@ -1,6 +1,11 @@
 # Add $HOME/.local/bin to path
 set PATH $HOME/.local/bin $PATH
 
+# Set vim as editor
+if which vim 2>&1 > /dev/null
+    set EDITOR (which vim)
+end
+
 # Set up arrow keys for history completion
 bind -k up history-token-search-backward
 bind -k down history-token-search-forward
